201c2d239a28361750bbc076deece61d4dc2ba5a
Donjetë Zogaj <70452463+Donjezo@users.noreply.github.com>
2026-04-02 20:44:50 +0200

    lesson 1

---
+++ lesson 1/main.py
@@ -0,0 +1,79 @@
+name ="Donjeta " # string
+surname = "Zogaj"
+
+full_name = name + surname
+age =21461 # int
+temperature = 98.2 #float
+
+
+
+
+
+
+
+
+
+
+
+
+
+users = ["LISA","Andi","Edeni","Olti"]
+
+print(users)
+
+
+personiFitues = users.pop()
+
+
+print(users)
+print(personiFitues)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#print(users)
+#users.insert(2,"Donjeta")
+
+#print(users)
+
+#print(users.index("Andi"))
+#print(users)
+#users.reverse()
+
+#print(users)
+#print(users)
+
+# users.append("Roberti")
+# print(users)
+# print(users.count("LISA"))
+#
+# users.clear()
+# print(users)
+
+#print(name)
+#print(temperature)
+#print(age)
+#print(full_name)
+#print(type(name))
+#print(type(age))
+
+#print(users[0])
+#print(users[1])
+#print(users[2])
+#print(users[3])
+
+#print(users)
