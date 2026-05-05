fc5eff455e646c4e7d1a7e35897dce4337099398
Donjetë Zogaj <70452463+Donjezo@users.noreply.github.com>
2026-05-05 20:49:54 +0200

    lesson 8

---
+++ lesson8/main.py
@@ -0,0 +1,27 @@
+from Personi import Personi
+from kerri import Kerri
+
+kerri1 = Kerri("Toyota",2019,"land..",00,"Japoni")
+kerri2 = Kerri("Tesla",2019,"land..",00,"Amerik")
+
+kerri1.ndalu()
+kerri1.rriteShpejtesin()
+
+kerri2.ndalu()
+
+print(kerri1.emri)
+print(kerri2.emri)
+
+kerri1.info()
+kerri2.info()
+
+
+personi1 = Personi("Donjeta",252,"F")
+personi2 = Personi("Robert",1254,"M")
+
+
+personi1.prezantimi()
+personi1.sayHi()
+
+personi2.prezantimi()
+personi2.sayHi()
